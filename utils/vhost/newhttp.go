c1763533da5051aa6c1b2469a4cff895f7a5b2e8
9sr <30464098+9sr@users.noreply.github.com>
2018-01-30 11:27:04 +0800

    Update newhttp.go

--- utils/vhost/newhttp.go
+++ utils/vhost/newhttp.go
@@ -25,8 +25,8 @@ import (
 	"sync"
 	"time"
 
-	frpLog "github.com/fatedier/frp/utils/log"
-	"github.com/fatedier/frp/utils/pool"
+	frpLog "github.com/9sr/frp/utils/log"
+	"github.com/9sr/frp/utils/pool"
 )
 
 var (
