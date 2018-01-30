c3d0294a7115a57e710636bfdf128cac80ef60e2
9sr <30464098+9sr@users.noreply.github.com>
2018-01-30 11:08:34 +0800

    Update control.go

--- client/control.go
+++ client/control.go
@@ -21,14 +21,14 @@ import (
 	"sync"
 	"time"
 
-	"github.com/fatedier/frp/models/config"
-	"github.com/fatedier/frp/models/msg"
-	"github.com/fatedier/frp/utils/crypto"
-	"github.com/fatedier/frp/utils/log"
-	frpNet "github.com/fatedier/frp/utils/net"
-	"github.com/fatedier/frp/utils/shutdown"
-	"github.com/fatedier/frp/utils/util"
-	"github.com/fatedier/frp/utils/version"
+	"github.com/9sr/frp/models/config"
+	"github.com/9sr/frp/models/msg"
+	"github.com/9sr/frp/utils/crypto"
+	"github.com/9sr/frp/utils/log"
+	frpNet "github.com/9sr/frp/utils/net"
+	"github.com/9sr/frp/utils/shutdown"
+	"github.com/9sr/frp/utils/util"
+	"github.com/9sr/frp/utils/version"
 	"github.com/xtaci/smux"
 )
 
